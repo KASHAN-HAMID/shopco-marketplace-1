2eed8929116ca47bba6f47d09dfc26205bb81f2d
Muhammad Ali <alisilat02@gmail.com>
2025-01-22 19:55:46 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,20 @@
-import dynamic from "next/dynamic";
+
 import Fonts from "@/components/fonts";
 import Hero from "@/components/Hero";
 import Dress from "@/components/dress";
+import TopSell from "./products/sell";
+import Products from "./products/page";
+import CustomerCarousel from "@/components/couresel";
 
 
-// Dynamically import components to improve performance
-const Products = dynamic(() => import("./products/page"), { ssr: false });
-const TopSell = dynamic(() => import("./products/sell"), { ssr: false });
-const CustomerCarousel = dynamic(() => import("@/components/couresel"), {
-  ssr: false,
-});
+
+
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Fonts />
-
       <Products />
 
       <TopSell />
